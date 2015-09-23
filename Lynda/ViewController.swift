ce7950f87470c51187324aad64c6f133eea85867
jim <jim@jims-macbook-air.local>
2015-09-23 18:10:44 -0400

    Initial Commit

---
+++ Lynda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lynda
+//
+//  Created by jim on 9/23/15.
+//  Copyright © 2015 Silver Connection LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
